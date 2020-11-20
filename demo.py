706b4c3f218919ea63b4bf13434913ad83b40309
mitchiezhao <mitchie.zhao12@gmail.com>
2020-11-20 12:37:26 +0800

    Add main function

--- demo.py
+++ demo.py
@@ -13,6 +13,11 @@ Options:
 
 from docopt import docopt
 opt = docopt(__doc__)
-print(opt)
-print(type(opt))
-print(opt["<arg4>"])
+
+def main(opt):
+    # print out dictionary of all arguments, type, and the new argument
+    print(opt)
+    print(type(opt))
+    print(opt["<arg4>"])
+
+main(opt)
